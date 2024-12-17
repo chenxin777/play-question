8b0e23b19a5a3e297dbca180d31907542ee4c476
chenxin777 <chenxinfang77@gmail.com>
2024-12-17 17:41:39 +0800

    后端初始化

---
+++ src/main/java/com/play/playquestion/mapper/PostThumbMapper.java
@@ -0,0 +1,15 @@
+package com.play.playquestion.mapper;
+
+import com.play.playquestion.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
